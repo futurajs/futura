dfed9a435cebde484e45dc63df4e277fed65973a
Ali Sabil <ali.sabil@gmail.com>
2018-10-03 15:33:24 +0200

    Fix the typing in the futura-html package

--- packages/html/src/index.ts
+++ packages/html/src/index.ts
@@ -1,132 +1,139 @@
-import { VNode, VAttribute, VElement, VText } from "@futura/virtual-dom";
+import { VNode, VElement, VText, VAttr, VEvent, VProp } from "@futura/virtual-dom";
 
-export type Content = ContentAtom | ContentArray;
-export type ContentAtom = VNode | string | undefined | null;
-export interface ContentArray extends Array<Content> {}
-
-export type Attributes<E extends HTMLElement> = Array<VAttribute<E> | undefined>;
 
 /** Plain text node */
 export const text = (content: string) =>
   new VText(content);
 
 /** Generic element */
-export const el = <E extends HTMLElement = HTMLElement>(tag: string, attributes: Attributes<E>, content: Content = []) =>
-  new VElement<E>(undefined, tag, vattributes(attributes), children(content));
+export const element = <E extends HTMLElement = HTMLElement>(tag: string, attrs: Attrs, content: Content = []) =>
+  new VElement<E>(undefined, tag, vattributes(attrs), children(content));
 
 /** Specific elements */
 
-export const a = element("a");
-export const abbr = element("abbr");
-export const address = element("address");
-export const area = element("area");
-export const article = element("article");
-export const aside = element("aside");
-export const audio = element("audio");
-export const b = element("b");
-export const bdi = element("bdi");
-export const bdo = element("bdo");
-export const blockquote = element("blockquote");
-export const br = element("br");
-export const button = element("button");
-export const canvas = element("canvas");
-export const caption = element("caption");
-export const cite = element("cite");
-export const code = element("code");
-export const col = element("col");
-export const colgroup = element("colgroup");
-export const data = element("data");
-export const datalist = element("datalist");
-export const dd = element("dd");
-export const del = element("del");
-export const details = element("details");
-export const dfn = element("dfn");
-export const dialog = element("dialog");
-export const div = element("div");
-export const dl = element("dl");
-export const dt = element("dt");
-export const em = element("em");
-export const embed = element("embed");
-export const fieldset = element("fieldset");
-export const figcaption = element("figcaption");
-export const figure = element("figure");
-export const footer = element("footer");
-export const form = element("form");
-export const h1 = element("h1");
-export const h2 = element("h2");
-export const h3 = element("h3");
-export const h4 = element("h4");
-export const h5 = element("h5");
-export const h6 = element("h6");
-export const header = element("header");
-export const hgroup = element("hgroup");
-export const hr = element("hr");
-export const i = element("i");
-export const iframe = element("iframe");
-export const img = element("img");
-export const input = element("input");
-export const ins = element("ins");
-export const kbd = element("kbd");
-export const label = element("label");
-export const legend = element("legend");
-export const li = element("li");
-export const main = element("main");
-export const map = element("map");
-export const mark = element("mark");
-export const menu = element("menu");
-export const menuitem = element("menuitem");
-export const meter = element("meter");
-export const nav = element("nav");
-export const object = element("object");
-export const ol = element("ol");
-export const optgroup = element("optgroup");
-export const option = element("option");
-export const output = element("output");
-export const p = element("p");
-export const param = element("param");
-export const picture = element("picture");
-export const pre = element("pre");
-export const progress = element("progress");
-export const q = element("q");
-export const rb = element("rb");
-export const rp = element("rp");
-export const rt = element("rt");
-export const rtc = element("rtc");
-export const ruby = element("ruby");
-export const s = element("s");
-export const samp = element("samp");
-export const section = element("section");
-export const select = element("select");
-export const small = element("small");
-export const source = element("source");
-export const span = element("span");
-export const strong = element("strong");
-export const sub = element("sub");
-export const summary = element("summary");
-export const sup = element("sup");
-export const table = element("table");
-export const tbody = element("tbody");
-export const td = element("td");
-export const textarea = element("textarea");
-export const tfoot = element("tfoot");
-export const th = element("th");
-export const thead = element("thead");
-export const time = element("time");
-export const tr = element("tr");
-export const track = element("track");
-export const u = element("u");
-export const ul = element("ul");
-export const var_ = element("var");
-export const video = element("video");
-export const wbr = element("wbr");
+export const a = el("a");
+export const abbr = el("abbr");
+export const address = el("address");
+export const area = el("area");
+export const article = el("article");
+export const aside = el("aside");
+export const audio = el("audio");
+export const b = el("b");
+export const bdi = el("bdi");
+export const bdo = el("bdo");
+export const blockquote = el("blockquote");
+export const br = el("br");
+export const button = el("button");
+export const canvas = el("canvas");
+export const caption = el("caption");
+export const cite = el("cite");
+export const code = el("code");
+export const col = el("col");
+export const colgroup = el("colgroup");
+export const data = el("data");
+export const datalist = el("datalist");
+export const dd = el("dd");
+export const del = el("del");
+export const details = el("details");
+export const dfn = el("dfn");
+export const dialog = el("dialog");
+export const div = el("div");
+export const dl = el("dl");
+export const dt = el("dt");
+export const em = el("em");
+export const embed = el("embed");
+export const fieldset = el("fieldset");
+export const figcaption = el("figcaption");
+export const figure = el("figure");
+export const footer = el("footer");
+export const form = el("form");
+export const h1 = el("h1");
+export const h2 = el("h2");
+export const h3 = el("h3");
+export const h4 = el("h4");
+export const h5 = el("h5");
+export const h6 = el("h6");
+export const header = el("header");
+export const hgroup = el("hgroup");
+export const hr = el("hr");
+export const i = el("i");
+export const iframe = el("iframe");
+export const img = el("img");
+export const input = el("input");
+export const ins = el("ins");
+export const kbd = el("kbd");
+export const label = el("label");
+export const legend = el("legend");
+export const li = el("li");
+export const main = el("main");
+export const map = el("map");
+export const mark = el("mark");
+export const menu = el("menu");
+export const menuitem = el("menuitem");
+export const meter = el("meter");
+export const nav = el("nav");
+export const object = el("object");
+export const ol = el("ol");
+export const optgroup = el("optgroup");
+export const option = el("option");
+export const output = el("output");
+export const p = el("p");
+export const param = el("param");
+export const picture = el("picture");
+export const pre = el("pre");
+export const progress = el("progress");
+export const q = el("q");
+export const rb = el("rb");
+export const rp = el("rp");
+export const rt = el("rt");
+export const rtc = el("rtc");
+export const ruby = el("ruby");
+export const s = el("s");
+export const samp = el("samp");
+export const section = el("section");
+export const select = el("select");
+export const small = el("small");
+export const source = el("source");
+export const span = el("span");
+export const strong = el("strong");
+export const sub = el("sub");
+export const summary = el("summary");
+export const sup = el("sup");
+export const table = el("table");
+export const tbody = el("tbody");
+export const td = el("td");
+export const textarea = el("textarea");
+export const tfoot = el("tfoot");
+export const th = el("th");
+export const thead = el("thead");
+export const time = el("time");
+export const tr = el("tr");
+export const track = el("track");
+export const u = el("u");
+export const ul = el("ul");
+export const var_ = el("var");
+export const video = el("video");
+export const wbr = el("wbr");
+
+
+/* Types */
+
+export type Content = ContentAtom | ContentArray;
+export type ContentAtom = Optional<VNode | string>;
+export interface ContentArray extends Array<Content> {}
+
+export type Attrs = ReadonlyArray<Optional<VAttr | VEvent | VProp>>;
+export type Attr = VAttr | VEvent | VProp;
+
+type Optional<T> = T | null | undefined;
 
 
 /* Helpers */
 
-function element<T extends keyof HTMLElementTagNameMap>(tag: T): (attributes: Attributes<HTMLElementTagNameMap[T]>, content?: Content) => VElement<HTMLElementTagNameMap[T]>;
-function element(tag: string): (attributes: Attributes<HTMLElement>, content?: Content) => VElement<HTMLElement>;
-function element(tag: string) {
-  return function (attributes: Attributes<HTMLElement>, content: Content = []) {
+function el<T extends keyof HTMLElementTagNameMap>(tag: T): (attrs: Attrs, content?: Content) => VElement<HTMLElementTagNameMap[T]>;
+function el(tag: string): (attributes: Attrs, content?: Content) => VElement<HTMLElement>;
+function el(tag: string) {
+  return function (attributes: Attrs, content: Content = []) {
     return new VElement(undefined, tag, vattributes(attributes), children(content));
   }
 }
@@ -155,11 +162,11 @@ function child(content: ContentAtom) {
   }
 }
 
-function vattributes<E extends HTMLElement>(attributes: Attributes<E>): ReadonlyArray<VAttribute<E>> {
-  const result: Array<VAttribute<E>> = [];
-  for (const attribute of attributes) {
-    if (attribute !== undefined) {
-      result.push(attribute);
+function vattributes(attrs: Attrs): ReadonlyArray<Attr> {
+  const result: Attr[] = [];
+  for (const attr of attrs) {
+    if (attr !== undefined && attr !== null) {
+      result.push(attr);
     }
   }
 
